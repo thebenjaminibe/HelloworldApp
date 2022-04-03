ec6dc2bca2d645f213ad95d74e661283f0d0db99
thebenjaminibe <frankynoble@gmail.com>
2022-04-03 15:21:32 +0100

    helloworldApp

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloworldApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World from Wellahealth school!");
+            Console.WriteLine("Welcome to week 1 of lectures");
+
+        }
+    }
+}
